3e5d713dc4d320de92980514636db93b662cd181
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2024-01-24 20:18:50 -0800

    Add vscode

---
+++ .chezmoiscripts/linux/run_onchange_after_vscode_plugins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension usernamehw.errorlens
+code --install-extension tamasfe.even-better-toml
+
+# Prettier
+code --install-extension esbenp.prettier-vscode
+code --install-extension bodil.prettier-toml
+
+# Language support
+code --install-extension leanprover.lean4
+code --install-extension mquandalle.graphql
+code --install-extension rust-lang.rust-analyzer
+code --install-extension svelte.svelte-vscode
+
+# Theming
+code --install-extension enkia.tokyo-night
