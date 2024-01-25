3d69a7c540c63a8a90c5a2c98e0919ca3f6d6656
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2024-01-24 21:18:32 -0800

    kitty

---
+++ .chezmoiscripts/linux/run_onchange_before_install_kitty.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+# Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
+# your system-wide PATH)
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
